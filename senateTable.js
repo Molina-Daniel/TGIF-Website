96b3100a6baa8c35d1a25b84ce2f03c66a737426
Daniel Molina <dmolcap@gmail.com>
2019-03-29 11:47:09 +0100

    function to build the table for senate

--- senateTable.js
+++ senateTable.js
@@ -1,37 +1,49 @@
 "use strict"
 
-document.getElementById("senateData").innerHTML = JSON.stringify(senateData, null, 2);
+// document.getElementById("senateData").innerHTML = JSON.stringify(senateData, null, 2);
 
 // senateData.results[0].members[0].first_name
 
 // coger el valor "x" del objeto senateData
-// crea una fila(tr)
-// crea una columna(td)
+// X crea una fila(tr) 
+// X crea una columna(td)
 // mete el valor "x" en la columna
 
-function getElement() {
-  let firstName = senateData.results[0].members[0].first_name;
+// function getElement() {
+//   let firstName = senateData.results[0].members[0].first_name;
+//   return firstName;
+// }
+// console.log(getElement(senateData));
+
+// Create rows and cells
+function rowsAndCellsCreator() {
+  let tBody = document.getElementById("senateTable");
+
+  let rows = tBody.insertRow(-1);
+
+  let cellFullName = rows.insertCell(0);
+  let cellParty = rows.insertCell(1);
+  let cellState = rows.insertCell(2);
+  let cellSeniority = rows.insertCell(3);
+  let cellVotesPercentage = rows.insertCell(4);
+
+  cellFullName.innerHTML = "Pepe";
+  cellParty.innerHTML = "R";
+  cellState.innerHTML = "TN";
+  cellSeniority.innerHTML = "11";
+  cellVotesPercentage.innerHTML = "85.97%";
+}
+
+rowsAndCellsCreator();
+
+// Get the data from the JSON
+function getTheName(fromHere) {
+  let firstName = fromHere.results[0].members[0].first_name;
   return firstName;
 }
-console.log(getElement(senateData));
 
-function makeTR() {
-  let makeTR = document.createElement("tr");
-  return makeTR;
-}
+console.log(getTheName(senateData));
 
-function makeTD() {
-  let makeTD = document.createElement("tr");
-  return makeTD;
-}
-
-function makeTable() {
-  let makeTR.innerHTML
-}
-
-function pushTheElement() {
-
-}
 
 
 // let makeTR = document.createElement("tr");
