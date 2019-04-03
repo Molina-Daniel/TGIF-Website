bbddb0a83c01125d01388b45e3cf1d6df4484f4a
Daniel Molina <dmolcap@gmail.com>
2019-04-03 16:37:42 +0200

    data filters for senate and house pages finished with no-data message

--- house-table.js
+++ house-table.js
@@ -1,121 +1,124 @@
-'use strict'
+"use strict"
 
-tableCreator();
+// addEventListener
 
+let members = houseData.results[0].members;
 
-// Get all the data from the JSON and execute the function to build the table:
+// Execute the functions to build the table:
+stateSelectOptionsCreator();
+tableCheck();
+// checkboxEvent();
+selectorEvent();
 
-function tableCreator() {
+function tableCheck() {
 
-  // Get the Full Name from the data:
+  let stateValue = document.getElementById("state").value;
+  let checkboxesArr = getCheckboxesValues();
+  let tBody = document.getElementById("houseTable");
+  tBody.innerHTML = "";
 
-  let fullNameArr = []; // Array holding all the full names
+  for (let i = 0; i < members.length; i++) {
+    // Check if the current member party is or not in the checkboxes values and create its row and cells if so
+    let checkboxesFilter = (checkboxesArr.length == 0 || checkboxesArr.includes(members[i].party));
+    let dropdownFilter = (stateValue == "-All-" || stateValue == members[i].state);
 
-  houseData.results[0].members.forEach(name => { // Loop to get first name, middle name(if exist) and last name, and build up the Full Name
-    let fullName;
-    fullName = name.first_name; // Get the first name
+    if (checkboxesFilter && dropdownFilter) {
+      // Create a new row
+      let rows = tBody.insertRow(-1);
 
-    if (name.middle_name !== null) {
-      fullName += " " + name.middle_name; // Get the middle name when exist and add it to the full name
+      // Create the full name cell
+      let cellFullName = rows.insertCell(0);
+
+      // Create the anchor for the names
+      let makeLink = document.createElement("a");
+      // Insert the attribute href and the url to the anchor tag
+      makeLink.setAttribute("href", members[i].url);
+      // Insert the attribute target blank
+      makeLink.setAttribute("target", "_blank");
+      // Insert the full name text in the anchor
+      makeLink.innerHTML = members[i].first_name + " " + (members[i].middle_name || "") + " " + members[i].last_name;
+      // Append the anchor tag to its cell
+      cellFullName.appendChild(makeLink);
+
+      // Create remaining cells
+      let cellParty = rows.insertCell(1);
+      let cellState = rows.insertCell(2);
+      let cellSeniority = rows.insertCell(3);
+      let cellVotesPercentage = rows.insertCell(4);
+
+      // Insert the correspondent values to each cell
+      cellParty.innerHTML = members[i].party;
+      cellState.innerHTML = members[i].state;
+      cellSeniority.innerHTML = members[i].seniority;
+      cellVotesPercentage.innerHTML = members[i].votes_with_party_pct + "%";
     }
+  }
+  let noData = document.getElementById("noData");
+  noData.innerHTML = "";
 
-    fullName += " " + name.last_name; // Get the last name and add it to the full name
-    fullNameArr.push(fullName); // Push the full name to the holder array
-  });
-
-
-  // Get the url from the data:
-
-  let fullNameWebArr = []; // Array storing all the urls
-
-  houseData.results[0].members.forEach(url => { // Loop to get the url of each member
-    let fullNameWeb;
-    fullNameWeb = url.url; // Get the url from the data
+  if (tBody.rows.length == 0) {
+    noData.insertAdjacentHTML("beforeend", `<p>Sorry, any data match your request.</p>`);
+  }
+  // TODO Make the noData message point with state has no data
+}
 
 
+// Create the options for the select filter
 
-    fullNameWebArr.push(fullNameWeb); // Push the url to the holder array
-  });
-
-
-  // Get the Party info from the data
-
-  let partyArr = []; // Array holding all the parties
-
-  houseData.results[0].members.forEach(party => { // Loop to get the party of each member
-    let eachParty = party.party; // Get the party
-
-    partyArr.push(eachParty); // Push the party info to the holder array
-  });
-
-
-  // Get the State info from the data
+function stateSelectOptionsCreator() {
 
   let stateArr = []; // Array holding all the States info
 
-  houseData.results[0].members.forEach(state => { // Loop to get the State of each member
+  members.forEach(state => { // Loop to get the State of each member
     let eachState = state.state; // Get the State
-
     stateArr.push(eachState); // Push the State info to the holder array
   });
 
+  // Create the no-duplicates array
+  let stateArrNoDuplicates = ["-All-"];
 
-  // Get the Seniority info from the data
-
-  let seniorityArr = []; // Array holding all the Seniority info
-
-  houseData.results[0].members.forEach(seniority => { // Loop to get the Seniority of each member
-    let eachSeniority = seniority.seniority; // Get the Seniority
-
-    seniorityArr.push(eachSeniority); // Push the Seniority info to the holder array
-  });
-
-
-  // Get the Percentage of Votes from the data
-
-  let votesPercentageArr = []; // Array holding all the porcentages of votes info
-
-  houseData.results[0].members.forEach(votesPct => { // Loop to get the porcentages of votes of each member
-    let eachVotesPct = votesPct.votes_with_party_pct + "%"; // Get the porcentages of votes and add % symbol
-
-    votesPercentageArr.push(eachVotesPct); // Push the porcentages of votes info to the holder array
-  });
-
-  rowsAndCellsCreator(fullNameArr, fullNameWebArr, partyArr, stateArr, seniorityArr, votesPercentageArr);
-}
-
-// Create rows and cells function
-
-function rowsAndCellsCreator(fullName, url, party, state, seniority, votesPercentage) {
-  let tBody = document.getElementById("houseTable");
-
-  for (let i = 0; i < fullName.length; i++) {
-    // Create a new row
-    let rows = tBody.insertRow(-1);
-
-    // Create the full name cell
-    let cellFullName = rows.insertCell(0);
-
-    // Create the anchor for the names
-    let makeLink = document.createElement("a");
-    // Insert the attribute href and the url to the anchor tag
-    makeLink.setAttribute("href", url[i]);
-    // Insert the full name text in the anchor
-    makeLink.innerHTML = fullName[i];
-    // Append the anchor tag to its cell
-    cellFullName.appendChild(makeLink);
-
-    // Create remaining cells
-    let cellParty = rows.insertCell(1);
-    let cellState = rows.insertCell(2);
-    let cellSeniority = rows.insertCell(3);
-    let cellVotesPercentage = rows.insertCell(4);
-
-    // Insert the correspondent values to each cell
-    cellParty.innerHTML = party[i];
-    cellState.innerHTML = state[i];
-    cellSeniority.innerHTML = seniority[i];
-    cellVotesPercentage.innerHTML = votesPercentage[i];
+  for (let i = 0; i < stateArr.length; i++) {
+    if (!stateArrNoDuplicates.includes(stateArr[i])) {
+      stateArrNoDuplicates.push(stateArr[i]);
+    }
   }
 
+  // Create an array with all the states sorted
+  let stateArrSelect = stateArrNoDuplicates.sort();
+
+  // Create the option list in the HTML
+  let targetSelect = document.getElementById("state");
+
+
+  for (let i = 0; i < stateArrSelect.length; i++) {
+    targetSelect.insertAdjacentHTML("beforeend", `<option value="${stateArrSelect[i]}">${stateArrSelect[i]}</option>`); // .insertAdjacentHTML parses the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
+  }
+}
+
+
+// Get checkedboxes value and put them into an array.
+function getCheckboxesValues() {
+  let checkboxesValue = [];
+  let checkboxes = document.querySelectorAll("input[name=partyCheckboxes]:checked");
+
+  checkboxesValue = Array.from(checkboxes).map(partyLetter => partyLetter.value);
+  return checkboxesValue;
+}
+
+
+// function checkboxEvent() {
+
+//   let checkboxChange = document.querySelectorAll("input[name=partyCheckboxes]");
+//   console.log(checkboxChange);
+//   let checkboxChangeArr = Array.from(checkboxChange);
+//   console.log(checkboxChangeArr);
+//   let checkboxChangeVal = checkboxChangeArr.map(checkboxValue => checkboxValue.value)
+//   console.log(checkboxChangeVal);
+//   // let checkboxChange = document.getElementById("democrat");
+//   checkboxChangeVal.addEventListener("click", tableCheck);
+// }
+
+function selectorEvent() {
+  let selectorChange = document.getElementById("state");
+  selectorChange.addEventListener("change", tableCheck);
 }
--- senate-table.js
+++ senate-table.js
@@ -1,159 +1,23 @@
 "use strict"
 
-// document.getElementById("senateData").innerHTML = JSON.stringify(senateData, null, 2);
-
-let fullNameArr = []; // Array holding all the full names
-let fullNameWebArr = []; // Array storing all the urls
-let partyArr = []; // Array holding all the parties
-let stateArr = []; // Array holding all the States info
-let seniorityArr = []; // Array holding all the Seniority info
-let votesPercentageArr = []; // Array holding all the porcentages of votes info
+// addEventListener
 
 let members = senateData.results[0].members;
 
-
-createTableChecked();
-tableCreator();
-
-
-// Get all the data from the JSON and execute the function to build the table:
-
-function tableCreator() {
-
-  members.forEach(name => { // Loop to get first name, middle name(if exist) and last name, and build up the Full Name
-    let fullName;
-    fullName = name.first_name; // Get the first name
-
-    if (name.middle_name !== null) {
-      fullName += " " + name.middle_name; // Get the middle name when exist and add it to the full name
-    }
-
-    fullName += " " + name.last_name; // Get the last name and add it to the full name
-    fullNameArr.push(fullName); // Push the full name to the holder array
-  });
-
-
-  // Get the url from the data:
-
-
-  members.forEach(url => { // Loop to get the url of each member
-    let fullNameWeb;
-    fullNameWeb = url.url; // Get the url from the data
-
-
-
-    fullNameWebArr.push(fullNameWeb); // Push the url to the holder array
-  });
-
-
-  // Get the Party info from the data
-
-
-
-  members.forEach(party => { // Loop to get the party of each member
-    let eachParty = party.party; // Get the party
-
-    partyArr.push(eachParty); // Push the party info to the holder array
-  });
-
-
-  // Get the State info from the data
-
-
-  members.forEach(state => { // Loop to get the State of each member
-    let eachState = state.state; // Get the State
-
-    stateArr.push(eachState); // Push the State info to the holder array
-  });
-
-
-  // Get the Seniority info from the data
-
-
-  members.forEach(seniority => { // Loop to get the Seniority of each member
-    let eachSeniority = seniority.seniority; // Get the Seniority
-
-    seniorityArr.push(eachSeniority); // Push the Seniority info to the holder array
-  });
-
-
-  // Get the Percentage of Votes from the data
-
-
-  members.forEach(votesPct => { // Loop to get the porcentages of votes of each member
-    let eachVotesPct = votesPct.votes_with_party_pct + "%"; // Get the porcentages of votes and add % symbol
-
-    votesPercentageArr.push(eachVotesPct); // Push the porcentages of votes info to the holder array
-  });
-
-  rowsAndCellsCreator();
-  // tableCheck();
-
-  stateSelectOptionsCreator();
-}
-
-
-
-
-// Create rows and cells function
-
-function rowsAndCellsCreator() {
-  let tBody = document.getElementById("senateTable");
-
-  for (let i = 0; i < members.length; i++) {
-
-    // Check if the current member party is or not in the checkboxes values and create its row and cells if so
-    // if (checkboxesArr.indexOf(members[i].party) >= 0)
-
-    // Create a new row
-    let rows = tBody.insertRow(-1);
-
-    // Create the full name cell
-    let cellFullName = rows.insertCell(0);
-
-    // Create the anchor for the names
-    let makeLink = document.createElement("a");
-    // Insert the attribute href and the url to the anchor tag
-    makeLink.setAttribute("href", members[i].url);
-    // Insert the attribute target blank
-    makeLink.setAttribute("target", "_blank");
-    // Insert the full name text in the anchor
-    makeLink.innerHTML = members[i].first_name + " " + (members[i].middle_name || "") + " " + members[i].last_name;
-    // Append the anchor tag to its cell
-    cellFullName.appendChild(makeLink);
-
-    // Create remaining cells
-    let cellParty = rows.insertCell(1);
-    let cellState = rows.insertCell(2);
-    let cellSeniority = rows.insertCell(3);
-    let cellVotesPercentage = rows.insertCell(4);
-
-    // Insert the correspondent values to each cell
-    cellParty.innerHTML = members[i].party;
-    cellState.innerHTML = members[i].state;
-    cellSeniority.innerHTML = members[i].seniority;
-    cellVotesPercentage.innerHTML = members[i].votes_with_party_pct + "%";
-
-
-  }
-
-}
-
-function createTableChecked() {
-  tableCheck()
-}
+// Execute the functions to build the table:
+stateSelectOptionsCreator();
+tableCheck();
+// checkboxEvent();
+selectorEvent();
 
 function tableCheck() {
 
   let stateValue = document.getElementById("state").value;
-
   let checkboxesArr = getCheckboxesValues();
-
   let tBody = document.getElementById("senateTable");
   tBody.innerHTML = "";
 
   for (let i = 0; i < members.length; i++) {
-
     // Check if the current member party is or not in the checkboxes values and create its row and cells if so
     let checkboxesFilter = (checkboxesArr.length == 0 || checkboxesArr.includes(members[i].party));
     let dropdownFilter = (stateValue == "-All-" || stateValue == members[i].state);
@@ -189,46 +53,28 @@ function tableCheck() {
       cellVotesPercentage.innerHTML = members[i].votes_with_party_pct + "%";
     }
   }
+  let noData = document.getElementById("noData");
+  noData.innerHTML = "";
+
+  if (tBody.rows.length == 0) {
+    noData.insertAdjacentHTML("beforeend", `<p>Sorry, any data match your request.</p>`);
+  }
+  // TODO Make the noData message point with state has no data
 }
 
-
-
-// Get checkedboxes value and put them into an array.
-function getCheckboxesValues() {
-  let checkboxesValue = [];
-
-  let checkboxes = document.querySelectorAll("input[name=partyCheckboxes]:checked");
-
-  checkboxesValue = Array.from(checkboxes).map(partyLetter => partyLetter.value);
-
-  return checkboxesValue;
-}
-
-
-// Call the main function tableCreator() whenever a checkbox is changed
-
-// function checkboxEvent() {
-
-//   // let checkboxChange = document.querySelectorAll("input[name=partyCheckboxes]:checked");
-
-//   return tableCreator();
-// }
-
-
-// function selectorEvent() {
-
-//   let selectorChange = document.getElementById("state");
-
-//   console.log(selectorChange);
-
-//   selectorChange.addEventListener("onchange", tableCreator());
-// }
 
 // Create the options for the select filter
 
 function stateSelectOptionsCreator() {
 
-  // Create an array with all the states sorted
+  let stateArr = []; // Array holding all the States info
+
+  members.forEach(state => { // Loop to get the State of each member
+    let eachState = state.state; // Get the State
+    stateArr.push(eachState); // Push the State info to the holder array
+  });
+
+  // Create the no-duplicates array
   let stateArrNoDuplicates = ["-All-"];
 
   for (let i = 0; i < stateArr.length; i++) {
@@ -237,19 +83,48 @@ function stateSelectOptionsCreator() {
     }
   }
 
-  // stateArrNoDuplicates.push("-All-");
+  // Create an array with all the states sorted
   let stateArrSelect = stateArrNoDuplicates.sort();
 
   // Create the option list in the HTML
   let targetSelect = document.getElementById("state");
 
-  let selectOptions = "";
 
   for (let i = 0; i < stateArrSelect.length; i++) {
-    selectOptions += `<option value="${stateArrSelect[i]}">${stateArrSelect[i]}</option>`;
+    targetSelect.insertAdjacentHTML("beforeend", `<option value="${stateArrSelect[i]}">${stateArrSelect[i]}</option>`); // .insertAdjacentHTML parses the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
   }
-  return targetSelect.innerHTML = selectOptions;
 }
+
+
+// Get checkedboxes value and put them into an array.
+function getCheckboxesValues() {
+  let checkboxesValue = [];
+  let checkboxes = document.querySelectorAll("input[name=partyCheckboxes]:checked");
+
+  checkboxesValue = Array.from(checkboxes).map(partyLetter => partyLetter.value);
+  return checkboxesValue;
+}
+
+
+// Call the main function tableCreator() whenever a checkbox is changed
+
+function checkboxEvent() {
+
+  let checkboxChange = document.querySelectorAll("input[name=partyCheckboxes]");
+  console.log(checkboxChange);
+  let checkboxChangeArr = Array.from(checkboxChange);
+  console.log(checkboxChangeArr);
+  let checkboxChangeVal = checkboxChangeArr.map(checkboxValue => checkboxValue.value)
+  console.log(checkboxChangeVal);
+  // let checkboxChange = document.getElementById("democrat");
+  checkboxChangeVal.addEventListener("click", tableCheck);
+}
+
+function selectorEvent() {
+  let selectorChange = document.getElementById("state");
+  selectorChange.addEventListener("change", tableCheck);
+}
+
 
 
 // let makeLink = document.createElement("a");
