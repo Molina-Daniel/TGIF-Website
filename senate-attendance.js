6a802f449e7a476a58ab83d95be36845fa9f9e1d
Daniel Molina <dmolcap@gmail.com>
2019-04-05 16:01:07 +0200

    task 3, senate attendance least engaged 10% done

--- senate-attendance.js
+++ senate-attendance.js
@@ -2,10 +2,11 @@
 
 let members = senateData.results[0].members;
 
-partyArraysBuilder();
+getPartiesData();
 tableGlance();
+leastEngagedCalc();
 
-function partyArraysBuilder() {
+function getPartiesData() {
   let democrats = [];
   let republicans = [];
   let independents = [];
@@ -49,3 +50,76 @@ function tableGlance() {
   document.getElementById("indVotPtc").innerHTML = Math.round(statistics.independents_pct * 100) / 100;
   document.getElementById("totalVotPtc").innerHTML = Math.round(statistics.total_pct * 100) / 100;
 }
+
+
+function compare(a, b) {
+  if (a.missed_votes_pct < b.missed_votes_pct)
+    return -1;
+  if (a.missed_votes_pct > b.missed_votes_pct)
+    return 1;
+  return 0;
+}
+
+function leastEngagedCalc() {
+  let membersSortedByMissVotesPct = members.sort(compare).reverse();
+  let missTenPct = [];
+  let leastEngagedTable = document.getElementById("leastEngagedTable");
+
+  for (let i = 0; i < membersSortedByMissVotesPct.length; i++) {
+    let member = members[i];
+
+    if (missTenPct.length <= membersSortedByMissVotesPct.length * 10 / 100) {
+      missTenPct.push(members[i]);
+      leastEngagedTable.insertAdjacentHTML("beforeend", `
+        <tr>
+          <td><a href="${member.url}" target="_blank">${member.first_name} ${member.middle_name || ""} ${member.last_name}</a></td>
+          <td>${member.missed_votes}</td>
+          <td>${member.missed_votes_pct}</td>
+        </tr>`);
+    }
+  }
+}
+
+
+
+// function leastEngagedCalc() {
+//   let missVotArr = [];
+
+//   for (let i = 0; i < members.length; i++) {
+//     missVotArr.push(members[i].missed_votes_pct);
+//   };
+
+//   let missVotSort = missVotArr.sort((a, b) => a - b).reverse();
+//   let missTenPct = [];
+
+//   for (let i = 0; i < members.length; i++) {
+//     let leastEngagedTable = document.getElementById("leastEngagedTable");
+//     // let isInTenPct = member.missed_votes_pct.includes(missVotArr);
+//     // let tenPctCondition = ((missTenPct.length <= (members.length * (10 / 100))) || members[i].missed_votes_pct == missTenPct[missTenPct.length - 1]);
+
+//     if (missTenPct.length <= (members.length * (10 / 100))) {
+//       for (let j = 0; j < members.length; j++) {
+//         let member = members[j];
+
+//         if ((member.missed_votes_pct == missVotSort[i]) || members[i].missed_votes_pct == missTenPct[missTenPct.length - 1]) {
+//           missTenPct.push(member);
+
+//           leastEngagedTable.insertAdjacentHTML("beforeend", `
+//             <tr>
+//               <td><a href="${member.url}" target="_blank">${member.first_name} ${member.middle_name ||
+//             ""} ${member.last_name}</a></td>
+//               <td>${member.missed_votes}</td>
+//               <td>${member.missed_votes_pct}</td>
+//             </tr>`);
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+
+
+
+
+
