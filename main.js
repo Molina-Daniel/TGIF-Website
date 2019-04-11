8e7f928b01594b81c66027e70fac6034af1a345f
Daniel Molina <dmolcap@gmail.com>
2019-04-11 12:18:35 +0200

    table at glance done with Vue for senate

--- main.js
+++ main.js
@@ -52,6 +52,10 @@ let app = new Vue({
     members: [],
     checkedParty: [],
     selectedState: ["All"],
+    // totalDem: [],
+    // totalRep: [],
+    // totalInd: [],
+
   },
   created() {
     this.fetchData();
@@ -71,7 +75,30 @@ let app = new Vue({
         return checkboxesFilter && dropdownFilter;
       })
     },
-
+    democrats() {
+      return this.members.filter(member => member.party == "D")
+    },
+    republicans() {
+      return this.members.filter(member => member.party == "R")
+    },
+    independents() {
+      return this.members.filter(member => member.party == "I")
+    },
+    demTotalVotes() { //Condiciones no funciona??
+      return (this.democrats.map(member => member.votes_with_party_pct).reduce((total, num) => total + num) / this.democrats.length).toFixed(2)
+      // if (this.members.filter(member => member.party == "D")) {
+      //   return this.members.map(member => member.votes_with_party_pct)//.reduce((total, num) => total + num)
+      // }
+    },
+    repTotalVotes() {
+      return (this.republicans.map(member => member.votes_with_party_pct).reduce((total, num) => total + num) / this.republicans.length).toFixed(2)
+    },
+    indTotalVotes() {
+      return (this.independents.map(member => member.votes_with_party_pct).reduce((total, num) => total + num) / this.independents.length).toFixed(2)
+    },
+    totalVotes() {
+      return (this.members.map(member => member.votes_with_party_pct).reduce((total, num) => total + num) / this.members.length).toFixed(2)
+    },
   },
   methods: {
     fetchData() {
@@ -107,3 +134,7 @@ let app = new Vue({
 
   },
 })
+
+// let numArr = [85.97, 81.45, 98.62, 91.86, 90.13, 95.72, 97.84, 98.63, 89.32, 98.81, 92.53, 98.04, 97.84, 91.38, 99.24, 99.24, 97.4, 97.65, 85.34, 82.86, 90.33, 86.12, 88.97, 59.67, 98.29, 85.8, 89.92, 96.1, 89.38, 87.6, 94.65, 98.93, 91.86, 98.72, 94.37, 82.68, 99.24, 98, 85.55, 92.85, 94.84, 98.73, 82.87, 98.01, 94.7, 83.98, 98.32, 91.71, 91.39, 86.37, 90.95, 98.47, 91.54, 97.84, 80, 95.4, 82.62, 98.62, 94.19, 96.97, 98.15, 85.96, 97.98, 85.87, 98.52, 84.66, 95.32, 91.74, 98.61, 96.61, 99.21, 90.85, 61.8, 98.01, 99.22, 98.59, 85.6, 88.47, 92.11, 97.86, 95.83, 88.51, 89.47, 98.43, 85.29, 94.95, 99.34, 98.93, 90.61, 88.98, 97.54, 87.5, 99.23, 94.62, 94.18, 90.24, 98.39, 98.17, 87.69, 97.64, 96.73, 97.67, 98, 90.16, 96.96];
+
+// let numArrSum = numArr.reduce((total, num) => total + num);
