f979d02517dcd38c0b6e3f7d2e44041b55319d67
Daniel Molina <dmolcap@gmail.com>
2019-04-12 13:22:43 +0200

    add loading page gif

--- main.js
+++ main.js
@@ -1,50 +1,5 @@
 'use strict'
 
-// let members;
-
-// apiToLoad();
-
-// function apiToLoad() {
-//   let senatePage = "http://localhost:8000/senate-page.html"
-//   let senateAttendancePage = "http://localhost:8000/senate-attendance.html"
-//   let senatePartyLoyaltyPage = "http://localhost:8000/senate-party-loyalty.html"
-//   let senateUrl = "https://api.propublica.org/congress/v1/113/senate/members.json";
-//   let houseUrl = "https://api.propublica.org/congress/v1/113/house/members.json";
-//   let url;
-
-//   if (document.URL == senatePage || document.URL == senateAttendancePage || document.URL == senatePartyLoyaltyPage) {
-//     url = senateUrl;
-//   } else {
-//     url = houseUrl;
-//   }
-
-//   fetch(url, {
-//     method: 'GET',
-//     headers: {
-//       'X-API-Key': 's3rUR0pNj1b3rAUOxF3Yt50ZRnneSpkQ11lVTwiq'
-//     },
-//   })
-//     .then(function (response) {
-//       if (!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       // Read the response as json.
-//       return response.json();
-//     })
-//     .then(function (responseAsJson) {
-//       // Do stuff with the JSON
-//       members = responseAsJson.results[0].members;
-//       // Execute the functions to build the table:
-//       stateSelectOptionsCreator();
-//       tableCheck();
-//       checkboxEvent();
-//       selectorEvent();
-//     })
-//     .catch(function (error) {
-//       console.log('Looks like there was a problem: \n', error);
-//     });
-// }
-
 
 let app = new Vue({
   el: "#app",
@@ -65,9 +20,6 @@ let app = new Vue({
   created() {
     this.fetchData();
   },
-  // mounted() {
-  //   this.leastEngagedTable();
-  // },
   computed: {
     states() { // Get all the states w/o repeats
       return new Set(this.members.map(member => member.state).sort())
@@ -175,6 +127,8 @@ let app = new Vue({
     mostLoyalTable() {
       this.mostLoyal = this.members.sort(this.compareLoyalty).reverse().slice(0, this.members.length * 10 / 100)
     },
+
   },
 })
+
 
